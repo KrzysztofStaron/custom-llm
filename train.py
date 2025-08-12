886ec928594c526f964c5c0f1b16251f6f6fc83b
Krzysztof Staroń <kisiel3141@gmail.com>
2025-08-13 00:21:25 +0200

    bigram generation - test

--- train.py
+++ train.py
@@ -1,8 +1,12 @@
+import torch
 from tokenizer import Tokenizer
 
 text = open("input.txt", "r").read()
+
 SPLIT_PERCENT = 0.9
 CONTEXT_LENGTH = 8
+BATCHE_SIZE = 4
+
 
 
 chars = sorted(list(set(text)))
@@ -11,18 +15,9 @@ vocab_size = len(chars)
 
 tokenizer = Tokenizer(chars)
 
-
-encoded = tokenizer.encode("hello")
-print(encoded)
-print(tokenizer.decode(encoded))
-
-
-import torch
-
 data = torch.tensor(tokenizer.encode(text))
 print(data.shape, data.dtype)
 print(data)
-
 
 
 n = int(SPLIT_PERCENT * len(data))
@@ -30,12 +25,9 @@ train_data = data[:n]
 val_data = data[n:]
 print(train_data.shape, val_data.shape)
 
-
-BATCHES = 4
-
 def get_batch(split):
   data = train_data if split == "train" else val_data
-  ix = torch.randint(len(data) - CONTEXT_LENGTH, (BATCHES,))
+  ix = torch.randint(len(data) - CONTEXT_LENGTH, (BATCHE_SIZE,))
   x = torch.stack([data[i:i+CONTEXT_LENGTH] for i in ix])
   y = torch.stack([data[i+1:i+CONTEXT_LENGTH+1] for i in ix])
   return x, y
@@ -44,7 +36,7 @@ def get_batch(split):
 x, y = get_batch("train")
 
 # Generating training sets from batches
-for b in range(BATCHES):
+for b in range(BATCHE_SIZE):
   for t in range(CONTEXT_LENGTH):
     context = x[b, :t+1]
     target = y[b, t]
@@ -53,10 +45,15 @@ for b in range(BATCHES):
 from bigramLM import BigramLanguageModel
 
 m = BigramLanguageModel(vocab_size)
-logits, loss = m(x, y)
-print(logits.shape, loss)
+optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
 
+for step in range(10000):
+    xb, yb = get_batch("train")
 
-
-
-
+    logits, loss = m(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+    
+print(loss.item())
+print(tokenizer.decode(m.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=300)[0].tolist()))
