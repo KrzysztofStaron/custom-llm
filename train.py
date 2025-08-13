e2436faeec6d3e9706b1a34e7e9fa1a03ead8e04
Krzysztof Staroń <kisiel3141@gmail.com>
2025-08-13 10:29:07 +0200

    slow average

--- train.py
+++ train.py
@@ -6,8 +6,11 @@ text = open("input.txt", "r").read()
 SPLIT_PERCENT = 0.9
 CONTEXT_LENGTH = 8
 BATCHE_SIZE = 4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+EVAL_ITERS = 200
+MAX_ITER = 3000
 
-
+torch.manual_seed(1337)
 
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
@@ -30,7 +33,22 @@ def get_batch(split):
   ix = torch.randint(len(data) - CONTEXT_LENGTH, (BATCHE_SIZE,))
   x = torch.stack([data[i:i+CONTEXT_LENGTH] for i in ix])
   y = torch.stack([data[i+1:i+CONTEXT_LENGTH+1] for i in ix])
+  x, y = x.to(DEVICE), y.to(DEVICE)
   return x, y
+
+@torch.no_grad()
+def estimate_loss():
+  losses = {}
+  m.eval()
+  for split in ["train", "val"]:
+    batch_losses = torch.zeros(EVAL_ITERS)
+    for k in range(EVAL_ITERS):
+      xb, yb = get_batch(split)
+      _, loss = m(xb, yb)
+      batch_losses[k] = loss.item()
+    losses[split] = batch_losses.mean().item()
+  m.train()
+  return losses
 
 # generating batches from train data
 x, y = get_batch("train")
@@ -44,10 +62,15 @@ for b in range(BATCHE_SIZE):
 
 from bigramLM import BigramLanguageModel
 
-m = BigramLanguageModel(vocab_size)
+m = BigramLanguageModel(vocab_size).to(DEVICE)
 optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
 
-for step in range(10000):
+# Training loop
+for step in range(MAX_ITER):
+    if step % EVAL_ITERS == 0:
+      losses = estimate_loss()
+      print(f"step {step}, train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
     xb, yb = get_batch("train")
 
     logits, loss = m(xb, yb)
@@ -56,4 +79,27 @@ for step in range(10000):
     optimizer.step()
     
 print(loss.item())
-print(tokenizer.decode(m.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=300)[0].tolist()))
+context = torch.zeros((1, 1), dtype=torch.long, device=DEVICE)
+print(tokenizer.decode(m.generate(context, max_new_tokens=300)[0].tolist()))
+
+B, T, C = 4, 8, 4
+x = torch.randn(B, T, C)
+
+print(x.shape)
+print(x)
+
+xbow = torch.zeros((B, T, C))
+for b in range(B):
+  for t in range(T):
+    xprev = x[b, :t+1]
+    xbow[b, t] = torch.mean(xprev, dim=0)
+
+
+
+
+
+
+
+
+
+
