f18cef9d0e852039a5439cb067e661ab76c28e09
krzysztofstaron <kisiel3141@gmail.com>
2026-06-08 22:47:34 +0200

    prep

--- bigramLM.py
+++
@@ -1,7 +0,0 @@
-
-
-
-
-# This create a Markov chain over tokens
-# It uses the past token to predict the next token
-# If we used a trigram we would use the past two tokens to predict the next token
